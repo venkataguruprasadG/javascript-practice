9218745307e378aee387e7c961b2f506d0304540
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-17 22:32:47 +0530

    else if : challenge 4.

---
+++ ControlFlow/if4.js
@@ -0,0 +1,14 @@
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade A");
+}
+else if (marks >= 75) {
+    console.log("Grade B");
+}
+else if (marks >= 50) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail")
+}
