2beb1f325111b37cb90de8a918f7bd79f1fe0a6e
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-17 22:25:15 +0530

    if : challenge 1

---
+++ ControlFlow/if.js
@@ -0,0 +1,11 @@
+let temperature = 35;
+
+if (temperature > 30) {
+    console.log("Hot");
+}
+else if (temperature > 20 && temperature < 30) {
+    console.log("Warm");
+}
+else {
+    console.log("Cold");
+}
