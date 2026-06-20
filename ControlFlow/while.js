cb345c97ccc39c219e5545dcf213396e8b18a3be
venkataguruprasadG <venkatagp27@gmail.com>
2026-06-20 21:14:32 +0530

    while : challenge

---
+++ ControlFlow/while.js
@@ -0,0 +1,20 @@
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+let k = 5;
+
+while (k >= 1) {
+    console.log(k);
+    k--;
+}
+
+let j = 2;
+
+while (j <= 20) {
+    console.log(j);
+    j += 2;
+}
